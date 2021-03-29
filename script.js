cd284baf84a4e33d954df3582080049029c3de6c
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-03-29 13:16:37 -0400

    fixed the basic Html layout in teh index.html will put the 5 day forecast cards in the script.js

--- script.js
+++ script.js
@@ -13,23 +13,30 @@ $.ajax({
 
 .then(function(data){
     var queryURL2= `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=5a5fc8508fe3901a1ad7e6702d8452ee&units=imperial`
-
+    var city = data.name;
+    
     $.ajax({
         url:queryURL2,
         method: "GET",
     })
     .then(function(data2){
+        var html = "";
         for(var i= 1; i< 6; i++){
-            var forecastedDays = data2.daily[i];
-            console.log(Date.datestring(forecastedDays.dt))
-
+            var day = data2.daily[i];
+            var forecastedDays = new Date(day.dt * 1000);
+            console.log(day)
+            var date = forecastedDays.toLocaleDateString();
+            //console.log(Date.datestring(forecastedDays.dt))
         }
-        
+        //inject HTML into the object. converts the js string into actual HTML elements
+        $(".bottom").html(html);
     })
 
     console.log(data)
     
 })
+
+
 
 
 
